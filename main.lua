a631b3b52214df2dffbcc5a6cbac3c7d3c39939f
tresks <55102988+tresks@users.noreply.github.com>
2022-03-16 14:25:57 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,2 @@
-getgenv().Players = game:GetService'Players'
-Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible = true
-Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Position = Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Position + UDim2.new(UDim.new(),Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Size.Y)
+game:GetService('Players').LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible = true
+game:GetService('Players').LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Position = Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Position + UDim2.new(UDim.new(),Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Size.Y)
